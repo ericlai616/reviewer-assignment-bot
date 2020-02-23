e93be465331a846d132b5f345689b467daed68cf
ericlai616 <25084249+ericlai616@users.noreply.github.com>
2020-02-24 00:26:17 +0900

    Use @octokit/webhooks

--- index.js
+++ index.js
@@ -36,73 +36,70 @@ const auth = createAppAuth({
   request: API_REQUEST
 });
 
+const WebhooksApi = require("@octokit/webhooks");
+const webhooks = new WebhooksApi({
+  secret: appConfig.get('secret')
+});
+
+webhooks.on('pull_request', async ({ id, name, payload }) => {
+  if (payload.action != 'labeled') {
+    log.debug('Skip non labeled actions');
+    return;
+  }
+  const labelName = payload.label.name;
+  if (!labelConfig.has(labelName)) {
+    log.debug('No action for label ' + labelName);
+    return;
+  }
+
+  const pullRequest = payload.pull_request;
+  const installationId = payload.installation.id;
+  const { token } = await auth({
+    type: "installation",
+    installationId: installationId,
+    permissions: {
+      "members": "read",
+      "pull_requests": "write"
+    }});
+  const requestWithAuthAndPrInfo = API_REQUEST.defaults({
+    headers: {
+      authorization: `token ${token}`
+    },
+    owner: pullRequest.head.repo.owner.login,
+    repo: pullRequest.head.repo.name,
+    pull_number: pullRequest.number
+  });
+
+  log.debug(`PR ${pullRequest.number}`);
+  requestWithAuthAndPrInfo("GET /repos/:owner/:repo/pulls/:pull_number/reviews").then(result => {
+    const currentReviewedReviewerLogins = result.data.users != null ? result.data.users.map(x => x.login) : [];
+    log.debug(`Current reviewed reviewers: ${currentReviewedReviewerLogins}`);
+
+    requestWithAuthAndPrInfo("GET /repos/:owner/:repo/pulls/:pull_number/requested_reviewers").then(result => {
+      const currentRequestedReviewerLogins = result.data.users != null ? result.data.users.map(x => x.login) : [];
+      log.debug(`Current requested reviewers: ${currentRequestedReviewerLogins}`);
+
+      const currentLabelConfig = labelConfig.get(labelName);
+      log.debug(`Load label config: ${JSON.stringify(currentLabelConfig)}`);
+      const nonCandidates = currentReviewedReviewerLogins.concat(currentRequestedReviewerLogins);
+      const reviewers = currentLabelConfig.reviewers;
+      const reviewerCandidates = reviewers.filter(x => x != pullRequest.user.login && !nonCandidates.includes(x));
+      const numOfReviewerToChoose = currentLabelConfig.has('number-of-reviewers') ? currentLabelConfig.get('number-of-reviewers') : reviewerCandidates.length;
+      log.debug(`Choose ${numOfReviewerToChoose} reviewer(s)`);
+      if (numOfReviewerToChoose > 0) {
+        let reviewersChosen = shuffle.pick(reviewerCandidates, { picks: numOfReviewerToChoose });
+        if (numOfReviewerToChoose == 1) {
+          reviewersChosen = [reviewersChosen];
+        }
+        log.debug(`Request review to ${reviewersChosen}`);
+        requestWithAuthAndPrInfo("POST /repos/:owner/:repo/pulls/:pull_number/requested_reviewers", {reviewers: reviewersChosen});
+      }
+    })
+  })
+})
 const EXPRESS_SERVER = express();
 EXPRESS_SERVER.use(express.json());
-EXPRESS_SERVER.post('/event_handler', async (req, res, next) => {
-  const event = req.header('X-GitHub-Event');
-  switch(event) {
-    case 'pull_request':
-      if (req.body.action != 'labeled') {
-        log.debug('Skip non labeled actions');
-        break;
-      }
-      if (!labelConfig.has(req.body.label.name)) {
-        log.debug('No action for label ' + req.body.label.name);
-        break;
-      }
-
-      const pullRequest = req.body.pull_request;
-      const installationId = req.body.installation.id;
-      const { token } = await auth({
-        type: "installation",
-        installationId: installationId,
-        permissions: {
-          "members": "read",
-          "pull_requests": "write"
-        }});
-      const requestWithAuthAndPrInfo = API_REQUEST.defaults({
-        headers: {
-          authorization: `token ${token}`
-        },
-        owner: pullRequest.head.repo.owner.login,
-        repo: pullRequest.head.repo.name,
-        pull_number: pullRequest.number
-      });
-
-      log.debug(`PR ${pullRequest.number}`);
-      requestWithAuthAndPrInfo("GET /repos/:owner/:repo/pulls/:pull_number/reviews").then(result => {
-        const currentReviewedReviewerLogins = result.data.users != null ? result.data.users.map(x => x.login) : [];
-        log.debug(`Current reviewed reviewers: ${currentReviewedReviewerLogins}`);
-
-        requestWithAuthAndPrInfo("GET /repos/:owner/:repo/pulls/:pull_number/requested_reviewers").then(result => {
-          const currentRequestedReviewerLogins = result.data.users != null ? result.data.users.map(x => x.login) : [];
-          log.debug(`Current requested reviewers: ${currentRequestedReviewerLogins}`);
-
-          const currentLabelConfig = labelConfig.get(req.body.label.name);
-          log.debug(`Load label config: ${JSON.stringify(currentLabelConfig)}`);
-          const nonCandidates = currentReviewedReviewerLogins.concat(currentRequestedReviewerLogins);
-          const reviewers = currentLabelConfig.reviewers;
-          const reviewerCandidates = reviewers.filter(x => x != pullRequest.user.login && !nonCandidates.includes(x));
-          const numOfReviewerToChoose = currentLabelConfig.has('number-of-reviewers') ? currentLabelConfig.get('number-of-reviewers') : reviewerCandidates.length;
-          log.debug(`Choose ${numOfReviewerToChoose} reviewer(s)`);
-          if (numOfReviewerToChoose > 0) {
-            let reviewersChosen = shuffle.pick(reviewerCandidates, { picks: numOfReviewerToChoose });
-            if (numOfReviewerToChoose == 1) {
-              reviewersChosen = [reviewersChosen];
-            }
-            log.debug(`Request review to ${reviewersChosen}`);
-            requestWithAuthAndPrInfo("POST /repos/:owner/:repo/pulls/:pull_number/requested_reviewers", {reviewers: reviewersChosen}).catch(next);
-          }
-        })
-        .catch(next);
-      })
-      .catch(next);
-      break;
-    default:
-  }
-  res.end('OK');
-})
-.get('/config', (req, res, next) => res.end(JSON.stringify(labelConfig)))
+EXPRESS_SERVER.use('/event_handler', webhooks.middleware);
 EXPRESS_SERVER.use((err, req, res, next) => {
   if (res.headersSent) {
     return next(err);
